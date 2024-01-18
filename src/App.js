e74edb92962cb889832af3a3eab4133c2d71f6ba
abdoelshafey20 <152815133+abdoelshafey20@users.noreply.github.com>
2024-01-18 02:16:01 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Header from './Header';
+import About from './About';
+import Device from './Device';
+import Age from './Age';
+import Download from './Download';
+import Get from './Get';
+import Footer from './Footer';
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Device/>
+      <Age/>
+      <Download/>
+      <Get/>
+      <Footer/>
+      
+
+    </div>
+  )
+}
+
+export default App;
